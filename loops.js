e85167c495fec01d776df8b7e844d594ca3b81a9
Michael Colgan <michaelwcolgan@gmail.com>
2018-10-16 20:24:19 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -16,13 +16,13 @@ function forLoop(array) {
     //var string1 = "I am 1 strange loop.";
     //var stringOther = "I am ${i} strange loops.";
     for(var i = 0; i < 25; i++) {
-     var string1 = "I am 1 strange loop.";
-     var stringOther = "I am " + i + " strange loops.";
+     var oneStrangeLoop = "I am 1 strange loop.";
+     var strangeLoops = "I am " + i + " strange loops.";
       if (i === 1) {
-      array.push(string1);
+      array.push(oneStrangeLoop);
     }
      else {
-      array.push(stringOther);
+      array.push(strangeLoops);
     }
  }
  return array;
